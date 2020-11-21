f0f99abd21f37dd8dc30f15d72d4af31e3d2672d
osha7 <ogroetz@gmail.com>
2020-11-21 16:07:41 -0700

    browser router back

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './css/App.css';
 import HeaderLandingPage from './container/header_landing_page';
 import MainPage from './container/main_page';
 import MovieInfo from './container/movie_info';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <HeaderLandingPage />
       <div className="pages">
-        <HashRouter basename={process.env.PUBLIC_URL} >
+        <BrowserRouter>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/movies/:id" component={MovieInfo} />
-        </HashRouter>
+        </BrowserRouter>
       </div>
     </div>
   );
